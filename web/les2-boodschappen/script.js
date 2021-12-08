4ed5db9c184050c187af75ec72e925b0acdf47f8
DaanDuits <daand04@gmail.com>
2021-12-08 12:45:33 +0100

    JavaScript toegevoegd

---
+++ web/les2-boodschappen/script.js
@@ -0,0 +1,21 @@
+const groentevak = document.getElementById("groente");
+const fruitvak = document.getElementById("fruit");
+
+function zetInFruitla(fruit)
+{
+    console.log(fruit);
+
+    const nieuwElement = document.createElement('h2');
+    nieuwElement.innerHTML = fruit;
+    nieuwElement.className = 'rood';
+    fruitvak.appendChild(nieuwElement);
+}
+function zetInGroentela(groente)
+{
+    console.log(groente);
+
+    const nieuwElement = document.createElement('h2');
+    nieuwElement.innerHTML = groente;
+    nieuwElement.className = 'groen';
+    groentevak.appendChild(nieuwElement);
+}
