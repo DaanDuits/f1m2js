d7925825484c376291bf4a2f1cbfea413b7dd072
DaanDuits <daand04@gmail.com>
2022-01-12 10:31:55 +0100

    les 3 gemaakt

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 
 [Opdracht 1](http://33815.hosts1.ma-cloud.nl/f1m2js/les1-background-color/)
 [Opdracht 2](http://33815.hosts1.ma-cloud.nl/f1m2js/les2-boodschappen/)
+[Opdracht 3](http://33815.hosts1.ma-cloud.nl/f1m2js/les3-tentoonstelling/)
+[Opdracht 4](http://33815.hosts1.ma-cloud.nl/f1m2js/les4-seasons/)
--- web/les3-tentoonstelling/script.js
+++ web/les3-tentoonstelling/script.js
@@ -1,1 +1,17 @@
-console.log('script');
+const myImage = document.getElementById("myImage")
+const myTitle = document.getElementById("myTitle")
+
+let titles = [
+    "De schilderijen van van Gogh",
+    "De rode wijngaardDe rode wijngaard",
+    "De sterrennacht",
+    "Sterrennacht boven de Rhône",
+    "Zonnebloemen",
+    "Boerderij in de Provence"
+];
+
+function ChangeImage(index)
+{
+    myImage.src = "img/painting" + index + ".jpg";
+    myTitle.innerHTML = titles[index]
+}
